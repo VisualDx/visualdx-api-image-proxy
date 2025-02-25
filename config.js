2e02af1ebe21b3eeddba0c15a2e7a808202d25b1
ltvuongqn88 <48003150+ltvuongqn88@users.noreply.github.com>
2025-02-25 17:55:55 +0700

    Update config.js
    oauth2-> auth

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
     API_BASE_URL: process.env.API_BASE_URL || "https://api-dev.visualdx.com/v1/libraries/images",
-    TOKEN_URL: process.env.TOKEN_URL || "https://api-dev.visualdx.com/v1/oauth2/token",
+    TOKEN_URL: process.env.TOKEN_URL || "https://api-dev.visualdx.com/v1/auth/token",
   
     // Audience setting: "consumer" or "clinical"
     AUDIENCE: process.env.AUDIENCE || "consumer",
